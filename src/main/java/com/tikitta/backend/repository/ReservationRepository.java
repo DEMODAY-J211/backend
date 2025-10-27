b515369944539e9cd4448e039a2587df827b1f90
jun77-s <mjun3712@gmail.com>
2025-10-27 19:32:02 +0900

    Update ReservationRepository.java

--- src/main/java/com/tikitta/backend/repository/ReservationRepository.java
+++ src/main/java/com/tikitta/backend/repository/ReservationRepository.java
@@ -1,10 +1,13 @@
 package com.tikitta.backend.repository;
 
-import com.tikitta.backend.domain.Reservation;
-import com.tikitta.backend.domain.ShowTime;
+import com.tikitta.backend.domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     List<Reservation> findAllByShowTime(ShowTime showTime);
