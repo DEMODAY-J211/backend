cdcb0df44572d718b02c8e236a2792b39ba6c411
Jeonhaechan <101245130+Jeonhaechan@users.noreply.github.com>
2025-11-07 16:17:13 +0900

    test

--- src/main/java/com/tikitta/backend/config/CustomOAuth2SuccessHandler.java
+++ src/main/java/com/tikitta/backend/config/CustomOAuth2SuccessHandler.java
@@ -43,7 +43,7 @@ public class CustomOAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHa
                 redirectUrl = frontendBaseUrl + "/manageshow";
             } else {
                 String relativePath = getDefaultUserRedirectUrl(request.getSession(false));
-                redirectUrl = frontendBaseUrl + "/homeuser";
+                redirectUrl = frontendBaseUrl + "/auth/kakao/callback";
             }
         }
 
@@ -76,7 +76,7 @@ public class CustomOAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHa
             return "/user/" + managerId + "/main";
         } else {
             // (예외 상황) 저장된 managerId가 없으면 테스트용 1번으로
-            return "/user/1/main"; // (또는 "/" 루트 페이지)
+            return "/auth/kakao/callback"; // (또는 "/" 루트 페이지)
         }
     }
 }
