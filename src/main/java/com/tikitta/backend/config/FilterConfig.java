4eca3f61f4e4ff6b838226e75f5e5bd689f05424
Jeonhaechan <101245130+Jeonhaechan@users.noreply.github.com>
2025-11-23 20:57:18 +0900

    fd

---
+++ src/main/java/com/tikitta/backend/config/FilterConfig.java
@@ -0,0 +1,19 @@
+package com.tikitta.backend.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.multipart.support.MultipartFilter;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<MultipartFilter> multipartFilter() {
+        FilterRegistrationBean<MultipartFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new MultipartFilter());
+        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE); // 가장 높은 우선순위 부여
+        return registrationBean;
+    }
+}
